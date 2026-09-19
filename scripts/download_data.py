88749d4d542b20e2bf91ad67191b25b9b18ed2bb
Divyanshu Gautam <tumhari-github-email@example.com>
2026-09-19 19:46:39 +0530

    Removed unneccesary comments

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,12 +1,9 @@
 import yfinance as yf
 
-# end date exclusive hoti hai, isliye 19 Sep dene se 18 Sep tak ka data aayega
 df = yf.download("GOOG", start="2019-01-01", end="2026-09-19", auto_adjust=False)
 
-# newer yfinance MultiIndex columns deta hai, usse simple columns me badlo
 df.columns = df.columns.get_level_values(0)
 
-# tumhare purane CSV wala column order
 df = df[["Open", "High", "Close", "Low", "Adj Close", "Volume"]]
 
 df.to_csv("data/raw_data/google_stock_price_raw.csv")
